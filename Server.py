f130a1640403bff8a46cd37522767bc00e02a208
gia-zheng55 <gia.zheng83@gmail.com>
2022-12-08 11:20:35 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,15 +1,17 @@
 import datetime
+import hashlib
 import math
 import asyncio
 import socket
+
+import post
 from Node import Node 
 from StatusCode import STATUS
 from Action import ACTION
 from Job import Job
-from http.server import HTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from http.server import CGIHTTPRequestHandler
 import sys
-from HTTPRequest import Resquest
 
 class Server(Node):
     numOfWorker = None  # number of workers
@@ -188,3 +190,53 @@ class Server(Node):
             except KeyboardInterrupt:
                 print("Keyboard interrupt closing requesting handling server.")
                 sys.exit(0)
+
+
+class Resquest(BaseHTTPRequestHandler, Server, Node):
+    timeout = 5
+    server_version = "Apache"  # 设置服务器返回的的响应头
+
+    index_content = '''
+    HTTP/ 1.x  200 ok
+    Content-Type: text/html
+    '''
+
+    file = open('index.html', 'r')
+    content = file.read()
+    file.close()
+
+    # def __init__(self, server, *args):
+    #     self.server = server
+    #     BaseHTTPRequestHandler.__init__(self, *args)
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")  # 设置服务器响应头
+        self.send_header("test1", "This is test!")  # 设置服务器响应头
+        self.end_headers()
+        self.wfile.write(self.content.encode())  # 里面需要传入二进制数据，用encode()函数转换为二进制数据   #设置响应body，即前端页面要展示的数据
+
+    def do_POST(self):
+        # 获取post提交的数据
+        datas = self.rfile.read(int(self.headers['content-length']))  # 固定格式，获取表单提交的数据
+        # datas = urllib.unquote(datas).decode("utf-8", 'ignore')
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")  # 设置post时服务器的响应头
+        self.send_header("test", "This is post!")
+        self.end_headers()
+
+        values = str(datas).split("&")
+        md5 = values[0].split("=")[-1]
+        worker = values[1].split("=")[-1]
+        hashMD5 = hashlib.md5(md5.encode()).hexdigest()
+        res = Server().test(hashMD5)
+        postCon = post.writeFile(md5, hashMD5, res)
+        self.wfile.write(postCon.encode())  # 提交post数据时，服务器跳转并展示的页面内容
+
+
+if __name__ == '__main__':
+    host = ('localhost', 8888)
+    server = HTTPServer(host, Resquest)
+    print("Starting server, listen at: %s:%s" % host)
+    server.serve_forever()
