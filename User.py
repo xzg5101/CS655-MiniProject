40deb3888c86d19a24b4829a57174cada3faf67c
xzg5101 <guoxiaoyang11@outlook.com>
2022-12-08 23:31:17 -0500

    feat: user now check input

--- User.py
+++ User.py
@@ -2,6 +2,8 @@ import hashlib
 import socket
 from Environment import DEBUG, SERVER_IP, REQUEST_PORT
 from Worker import Worker
+from Node import Node
+import time
 
 IP = '127.0.0.1'
 PORT = 50010
@@ -13,9 +15,16 @@ serverIP = SERVER_IP
 if DEBUG == 1:
     serverIP = '127.0.0.1'
 
+aNode = Node()
 
-aStr = 'BAAAc'
-locMd5 = hashlib.md5(aStr.encode()).hexdigest()
+print("Please enter a string of length 5 only contains [a-zA-Z]:")
+aInput = input()
+
+while not aNode.verifyPassword(aInput):
+    print(f"Input format is wrong [{aInput}], please re-enter")
+    aInput = input()
+
+locMd5 = hashlib.md5(aInput.encode()).hexdigest()
 
 msg = f"cra {locMd5}"
 
@@ -23,11 +32,14 @@ def query():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((serverIP, REQUEST_PORT))
     print("connecting with ", SERVER_IP)
+
+    time1 = time.time()
     s.sendall(msg.encode('utf-8'))
     data = b""
     data += s.recv(1024)
+    time2 = time.time()
     ans = data.decode().split()
-    print(f"received result [{ans[2]}], is it correct?", ans[2] == aStr)
-    print(f"spent [{ans[3]}] seconds")
+    print(f"received result [{ans[2]}], is it correct?", ans[2] == aInput)
+    print(f"Server spent [{ans[3]}] seconds, user waited [{time2-time1}] seconds")
 
 query()
